e3d67f4ad7876af48ecf18c7dd46aefb17d6befc
Balamurugan-d <balamurugan.d@ssi.safestart.com>
2020-04-25 15:41:52 +0530

    Updates

---
+++ precache-manifest.fd3ffbe7c5806e5b395c2d84a0114e96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ace92b5f07ec55f3e42933b54fbcadcc",
+    "url": "/expenseplanner/index.html"
+  },
+  {
+    "revision": "132262b5c3404e1bb46e",
+    "url": "/expenseplanner/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "4f1b63fd38349a03160a",
+    "url": "/expenseplanner/static/css/main.2ab1c548.chunk.css"
+  },
+  {
+    "revision": "132262b5c3404e1bb46e",
+    "url": "/expenseplanner/static/js/2.efa04ed2.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/expenseplanner/static/js/2.efa04ed2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f1b63fd38349a03160a",
+    "url": "/expenseplanner/static/js/main.1f540a23.chunk.js"
+  },
+  {
+    "revision": "76b1be62ff32c93b5f20",
+    "url": "/expenseplanner/static/js/runtime-main.4a3282e1.js"
+  }
+]);
